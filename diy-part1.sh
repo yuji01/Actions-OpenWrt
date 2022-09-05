8943c30cbd8acc70473d74f73ecad3f9dd80fe89
yuji01 <59014140+yuji01@users.noreply.github.com>
2022-09-05 09:08:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
+echo 'src-git KyleRicardo https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk' >>feeds.conf.default
