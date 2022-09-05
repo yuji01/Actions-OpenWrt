9dcdda16d8cf67fda5bb189a60bdc4504ecea220
yuji01 <59014140+yuji01@users.noreply.github.com>
2022-09-05 11:23:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+#加载自定义配置之后
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
